a5c97b74c7c1ccda8dfaf21ef92acff295dc6fe4
Bartosz Kalemba <kalemba128@gmail.com>
2024-04-16 18:03:09 +0200

    upgrade_dependencies - fix

--- packages/core/lib/src/util/context_extensions.dart
+++ packages/core/lib/src/util/context_extensions.dart
@@ -5,9 +5,14 @@ import 'package:utopia_cms/src/theme/cms_theme_data.dart';
 import 'package:utopia_cms/src/theme/cms_theme_text_styles.dart';
 
 extension CmsContextExtensions on BuildContext {
-  CmsThemeData get theme => get<CmsThemeData?>() ?? CmsThemeData.defaultTheme;
+  CmsThemeData get theme {
+    final data = getUnsafe(CmsThemeData) as CmsThemeData?;
+    return data ?? CmsThemeData.defaultTheme;
+  }
 
   CmsThemeColors get colors => theme.colors;
+
   CmsThemeTextStyles get textStyles => theme.textStyles;
+
   BoxDecoration get fieldDecoration => theme.fieldDecoration;
 }
